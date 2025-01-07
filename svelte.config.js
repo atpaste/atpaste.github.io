c88d0971e5a885fd506d2b4328bf96ebb2d366b6
uwx <maxine@cybergal.com>
2025-01-06 22:15:23 -0300

    Add spa.yml and svelte-static config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+// @ts-check
+
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @satisfies {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -11,7 +13,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'dist',
+			assets: 'dist',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
