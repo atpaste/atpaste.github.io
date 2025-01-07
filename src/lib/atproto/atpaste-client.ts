80060137a11c65b907c6f19c781015646c1a30a3
uwx <maxine@cybergal.com>
2025-01-07 15:49:29 -0300

    use ShortIds as rkeys, use new AtUri from kitty-agent

--- src/lib/atproto/atpaste-client.ts
+++ src/lib/atproto/atpaste-client.ts
@@ -1,4 +1,4 @@
-import { type KittyAgent, getSha256 } from 'kitty-agent';
+import { type KittyAgent, getSha256, ShortId } from 'kitty-agent';
 import type { At } from '@atcute/client/lexicons';
 import { now as tidNow } from '@atcute/tid';
 import { compress } from '$lib/zlib';
@@ -68,7 +68,8 @@ export class AtpasteClient {
 
         const blob = await this.agent.uploadBlob(new Blob([pageBinary], { type: mimeType }));
 
-        const rkey = tidNow();
+        const rkey = ShortId.now();
+        const date = new Date().toISOString();
 
         const result = await this.agent.put({
             collection: 'blue.zio.atfile.upload',
@@ -80,11 +81,11 @@ export class AtpasteClient {
                 blob: blob,
                 file: {
                     mimeType: 'application/vnd.age',
-                    modifiedAt: new Date().toISOString(),
+                    modifiedAt: date,
                     name: rkey,
                     size: pageBinary.length
                 },
-                createdAt: new Date().toISOString(),
+                createdAt: date,
                 finger: {
                     $type: 'blue.zio.atfile.finger#browser',
                     id: 'io.github.atpaste.paste',
@@ -109,7 +110,7 @@ export class AtpasteClient {
         return uploads
             .filter(upload => upload.value.finger?.id === 'io.github.atpaste.paste')
             .map(upload => ({
-                rkey: new AtUri(upload.uri).rkey,
+                rkey: upload.uri.rkey,
                 blob: upload.value.blob,
                 cid: upload.cid,
                 isEncrypted: upload.value.file?.mimeType === 'application/vnd.age'
